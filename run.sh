f5f765b871e685553a4d79db09d07d435ac94009
xyz <15109268895x@gmail.com>
2024-04-14 14:01:01 -0400

    modify run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
+# add stages 
 dvc stage add --force -n prepare \
     -d prepare.py -d data/vineyard_weather_1948-2017.csv \
     -o data/prepared.csv \
@@ -12,11 +13,16 @@ dvc stage add --force -n featurize \
 
 dvc stage add --force -n train \
     -d train.py \
+    -d params.yaml \
     -d data/train_data.csv \
     -o model/model.pkl \
     python train.py
 
 dvc stage add --force -n evaluate \
     -d evaluate.py -d model/model.pkl -d data/test_data.csv \
+    -d params.yaml \
     -o results/evaluation_report.txt \
     python evaluate.py
+
+# reproduce experiments from the DVC pipeline. This will execute the steps defined in your dvc.yaml file.
+dvc exp run
